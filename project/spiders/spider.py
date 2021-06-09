ebca878e84d5c55f41668e5478761cb5c30f3ec1
Pravin-predator <61304004+Pravin-predator@users.noreply.github.com>
2021-06-09 11:25:45 +0530

    Add files via upload

---
+++ project/spiders/spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class ToScrapeCSSSpider(scrapy.Spider):
+    name = "toscrape-css"
+    start_urls = [
+        'http://quotes.toscrape.com/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+
+            }
+

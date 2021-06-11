fac0a491985817baf320de875b13bd676959046a
Pravin-predator <61304004+Pravin-predator@users.noreply.github.com>
2021-06-11 13:50:06 +0530

    Update pipelines.py

--- project/pipelines.py
+++ project/pipelines.py
@@ -6,8 +6,40 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
+import mysql.connector
 
 
-class WebscrapePipeline:
+class WebscrapePipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            passwd='Kumar@pravin 007',
+            database='mydb'
+        )
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""create table quotes_tb(
+                            text text,
+                            author text,
+                            tags text
+                            )""")
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        self.curr.execute("""insert into quotes_tb value(%s,%s,%s)""", (
+            item['text'][0],
+            item['author'][0],
+            item['tags'][0]
+        ))
+        self.conn.commit()
